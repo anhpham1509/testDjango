8f10254f970019ecc3ed872eab81a2892e9f6d49
Anh Pham <duyanhpham1509@gmail.com>
2015-09-18 20:24:15 +0300

    complete templates and static

--- tutDjango/rango/views.py
+++ tutDjango/rango/views.py
@@ -3,9 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("<h1>Duy Anh says hello to all of you guys! ;-)</h1> <a href='about/'>About page</a>")
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict={'boldmessage':"The bold text in the content"}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
     return HttpResponse("<h2>Rango says here is the about page.</h2> <a href='..'>Homepage</a>")
