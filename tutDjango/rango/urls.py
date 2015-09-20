02b3a684c0b49a0fa9888120c98e239c014dbf0d
Anh Pham <duyanhpham1509@gmail.com>
2015-09-20 13:46:59 +0300

    complete chapter 7

--- tutDjango/rango/urls.py
+++ tutDjango/rango/urls.py
@@ -3,7 +3,6 @@ __author__ = 'DuyAnhPham'
 from django.conf.urls import patterns, url
 from rango import views
 
-urlpatterns = [url(r'^$', views.index, name = 'index'),
-               url(r'^about/', views.about, name = 'about'),
-               ]
-#urlpatterns = patterns(url(r'^about/', views.index, name = 'about'))
+urlpatterns = [url(r'^$', views.index, name='index'),
+               url(r'^about/', views.about, name='about'),
+               url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),]
